c5e34c900957554e01f58cb4a0a12b87fd45b449
Merak <mesheryakov97@gmail.com>
2020-09-27 23:10:08 +0500

    feat: enter

--- app/js/common.js
+++ app/js/common.js
@@ -38,4 +38,10 @@ $(function () {
     //   },
     // },
   });
+
+  // popup
+  $(".open-popup").magnificPopup({
+    type: "inline",
+    midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ gulp.task("js", function () {
         "app/libs/jquery/dist/jquery.min.js",
         "app/libs/swiper/swiper.min.js",
         "app/libs/magnific-popup/jquery.magnific-popup.min.js",
+        "app/libs/fancybox/jquery.fancybox.min.js",
         "app/js/common.js", // Always at the end
       ])
       .pipe(concat("scripts.min.js"))
